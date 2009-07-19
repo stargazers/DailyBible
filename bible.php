c654d6043e455cadff205f22c7e764e49b5d6383
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2009-07-19 17:25:14 +0300

    Lisätty --help
     Nyt ohjelmassa on lyhyt ohje --help parametrillä.

--- bible.php
+++ bible.php
@@ -1,5 +1,28 @@
 <?php
 	
+	function show_help( $argv )
+	{
+		echo "DailyBible\n\n";
+		echo "Näyttää päivän Raamattulauseen käyttäen sivuston\n"
+			. "http://raamattu.uskonkirjat.net sisältöä\n\n";
+		echo "Käyttö: " . basename( $argv[0] ) . " [PARAMS]\n\n";
+		echo "Mahdolliset parametrit:\n";
+		echo " --whole_chapter\n";
+		echo "   Näyttää koko luvun\n";
+		echo " --language english\n";
+		echo "   Näyttää Päivän Sanan englanniksi. Muut vaihtoehdot ovat\n";
+		echo "   finnish (oletus) ja old_finnish\n";
+		echo " --only_once\n";
+		echo "   Näyttää vain kerran päivässä Päivän Sanan. Tämä on\n";
+		echo "   tarkoitettu jos halutaan lisätä ohjelma .bashrc tai\n";
+		echo "   vastaavan loppuun.\n";
+		echo " --help\n";
+		echo "   Näyttää tämän ohjeen.\n";
+		echo "\n";
+		echo "Ohjelmoinut: Aleksi Räsänen <aleksi.rasanen@runosydan.net>\n";
+		die();
+	}
+
 	include 'CBible.php';
 	$x = new CBible();
 
@@ -11,6 +34,11 @@
 	{
 		switch( $argv[$i] )
 		{
+			// User needs to get params
+			case '--help':
+				show_help( $argv );
+				break;
+
 			// User wants to use another language
 			case '--language':
 				if( isset( $argv[$i+1] ) )
