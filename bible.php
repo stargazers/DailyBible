1bd4eca29733d36b9dbca713430bc7f950298d16
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2009-07-19 13:11:00 +0300

    Lisätty parametri --only_once
     Lisätty parametri --only_once joka mahdollistaa että ohjelma
     näyttää Päivän Sanan vain kerran päivässä. Tätä voi siis käyttää
     kun halutaan lisätä ohjelma käynnistymään .zshrc tai .bashrc tiedostoon
     loppuun tai vastaavaa.
    
     Nyt lisätty myös dokumentaatiota.

--- bible.php
+++ bible.php
@@ -3,10 +3,41 @@
 	include 'CBible.php';
 	$x = new CBible();
 
-	if( $argc > 1 )
+	// Read home dir with exec... maybe there is better way?
+	$path = exec( 'echo $HOME' );
+
+	// Check command line arguments.
+	for( $i=0; $i < $argc; $i++ )
 	{
-		if( $argv[1] == '--whole_chapter' )
-			$x->whole_chapter( true );
+		switch( $argv[$i] )
+		{
+			// User wanted the whole chapter
+			case '--whole_chapter':
+				$x->whole_chapter( true );
+				break;
+
+			// User wants to see Daily Verse only once in a day.
+			case '--only_once':
+				$only_once = true;
+
+				// Temporary file
+				$tmp = $path . '/.bible.txt';
+
+				// Is there temporary file? If so, then
+				// just check if there is this day in that temporary
+				// file. If there is, just die then, because Daily Verse
+				// is already shown today.
+				if( file_exists( $tmp ) )
+				{
+					$data = file( $tmp );
+					if( isset( $data[0] ) )
+					{
+						if( $data[0] == date('Y-m-d' ) )
+							die();
+					}
+				}
+				break;
+		}
 	}
 
 	$ret = $x->get_verse();
@@ -21,5 +52,14 @@
 			echo $val . "\n";
 	}
 
-	//print_r( $ret );
+	// If user wants Daily Verse only once in a day, then we must
+	// create temporary file so next time this script is called we
+	// don't show Daily Verse again.
+	if( $only_once )
+	{
+		$fh = fopen( $path . '/.bible.txt', 'w' );
+		fwrite( $fh, date( 'Y-m-d' ) );
+		fclose( $fh );
+	}
+
 ?>
